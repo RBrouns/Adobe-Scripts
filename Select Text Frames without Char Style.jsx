8bff0c3059a0036898f5bec486a53ebe66dcbdda
Brouns <robin.brouns@gtherapeutics.com>
2017-10-07 21:09:19 +0200

    Fixed bug in Unstyled text frame selector script

--- Select Text Frames without Char Style.jsx
+++ Select Text Frames without Char Style.jsx
@@ -5,15 +5,23 @@ $.writeln("Existing styles:" + doc.characterStyles.length);
 $.writeln("Existing paragraph styles:" + doc.paragraphStyles.length);
 
 var unstyledTextfields = 0;
+var unstyledTfOnLockedLayer = 0;
 
 for(var i=0;i<doc.textFrames.length;i++){
-    var tf = doc.textFrames[i];
-    if(tf.appliedCharacterStyle == undefined && tf.appliedCharacterStyle == undefined){
-        tf.selected = true;
+    var textRange = doc.textFrames[i].textRange; 
+    if(!hasCharacterStyle(textRange)){
+        if(!doc.textFrames[i].layer.locked){
+            doc.textFrames[i].selected = true;
+        }else{
+            unstyledTfOnLockedLayer++;
+        }
         unstyledTextfields++;
-    }else{ 
-        $.writeln(doc.textFrames[i].appliedCharacterStyle.toString());
     }
 }
 
-alert("Unstyled textfields: " + unstyledTextfields);
+function hasCharacterStyle(textRange){
+    var hasStyle = textRange.characterStyles != undefined && textRange.characterStyles[0].toString().indexOf("Normal Character Style") == -1
+    return hasStyle;
+ }
+
+alert("Unstyled textfields: " + unstyledTextfields + ", out of total: " + doc.textFrames.length + ". " + (unstyledTfOnLockedLayer!=0? "\nWARNING! " + unstyledTfOnLockedLayer + " unstyled textfields were on locked layers" : "All fields selected"));
